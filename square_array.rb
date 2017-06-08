8bebf06898cd7e5311165c2ff4f049ab2c364583
Luke Pierotti <lpierotti2@gmail.com>
2017-06-08 19:37:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  finalArray = []
+  array.each do |num|
+    finalArray.push(num ** 2)
+  end
+  finalArray
+  # array.collect do |num|
+  #   num * num
+  # end
 end
